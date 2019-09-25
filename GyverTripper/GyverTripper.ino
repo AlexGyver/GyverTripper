2dcec24580892ab1a4730f7ba8572b449b195f42
Alex <beragumbo@ya.ru>
2019-09-25 13:17:08 +0300

    add

---
+++ GyverTripper/GyverTripper.ino
@@ -0,0 +1,52 @@
+/*
+  Скетч к проекту "Дневной Экскурсант"
+  Страница проекта (схемы, описания): https://alexgyver.ru/GyverTripper/
+  Исходники на GitHub: https://github.com/AlexGyver/GyverTripper/
+  Нравится, как написан код? Поддержи автора! https://alexgyver.ru/support_alex/
+  Автор: AlexGyver, AlexGyver Technologies, 2019
+  https://AlexGyver.ru/
+*/
+
+#define FREQ 10               // частота генератора отправки данных
+#define KEY_PRESS KEY_D       // какая кнопка будет нажата
+#define MODIFIER MOD_GUI_LEFT // модификатор (кнопка win (gui))
+
+#define ATTEMPTS 3      // количество попыток распознать
+#define DEADZONE 8000   // мёртвая зона
+#define CLOSE_TIMEOUT 10000 // таймаут срабатывания
+#define RADIO_PIN 0
+
+#include "DigiKeyboard.h"
+
+byte counter = 0;
+uint32_t pulse = (long)1000000 / FREQ / 2;
+uint32_t toutTimer;
+
+void setup() {
+  //Serial.begin(9600);
+}
+
+void loop() {
+  long thisPulse = pulseIn(RADIO_PIN, HIGH, (long)pulse * 3);
+  if (thisPulse < pulse + DEADZONE &&
+      thisPulse > pulse - DEADZONE) {
+    if (++counter >= ATTEMPTS) {
+      //Serial.println("good!");
+      if (millis() - toutTimer >= CLOSE_TIMEOUT) {
+        toutTimer = millis();
+        palundra();
+      }
+      counter = 0;
+    }
+  } else {
+    //Serial.println("bad!");
+    counter = 0;
+  }
+  //Serial.println(pulseIn(2, HIGH, (long)pulse * 3));
+}
+
+void palundra() {
+  DigiKeyboard.sendKeyPress(KEY_PRESS, MODIFIER);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyPress(0);
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # GyverTripper
- Сигнализация-сворачиватель окон на ATtiny85
+Сигнализация - сворачиватель окон на Digispark
+
+## Схемы
+![SCHEME](https://github.com/AlexGyver/GyverTripper/blob/master/schemes/scheme1.jpg)
+
+## Материалы и компоненты
+### Ссылки оставлены на магазины, с которых я закупаюсь уже не один год
+* Digispark https://ali.ski/_hQBsm  https://ali.ski/IowM2
+* Радио https://ali.ski/CZZqQ https://ali.ski/KrT_iW
+* Генератор https://ali.ski/VyCPV  https://ali.ski/by098
+* PIR https://ali.ski/1169z  https://ali.ski/X1RbuZ
+* Транзисторы https://ali.ski/a89ia6
+* Резисторы 220R https://ali.ski/LefDUN
+* Отсек для батарейки https://ali.ski/uJSTbT  https://ali.ski/0_XUoH
+
+## Вам скорее всего пригодится
+* [Всё для пайки (паяльники и примочки)](http://alexgyver.ru/all-for-soldering/)
+* [Недорогие инструменты](http://alexgyver.ru/my_instruments/)
+* [Все существующие модули и сенсоры Arduino](http://alexgyver.ru/arduino_shop/)
+* [Электронные компоненты](http://alexgyver.ru/electronics/)
+* [Аккумуляторы и зарядные модули](http://alexgyver.ru/18650/)
+
+### Как использовать
+Читать вот этот гайд https://alexgyver.ru/arduino-first/
+
